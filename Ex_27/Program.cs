a8962cfe82d62782ef38d650788496beebae0667
Maria Kalina <kalina2908@mail.ru>
2022-12-10 23:49:33 +0300

    add ex_27

---
+++ Ex_27/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод.");
+        }
+    }
+    return result;
+}
+
+int GetSum (int number)
+{
+    int result=0;
+    while (number>0)
+    {
+        result=result+number%10;
+        number=number/10;
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число:");
+int res = GetSum(number);
+Console.WriteLine(res);
